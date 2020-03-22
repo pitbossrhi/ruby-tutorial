0bef3a2e9250d0346fe1fa26ca4380893fdaea1e
pitbossrhi <62507319+pitbossrhi@users.noreply.github.com>
2020-03-22 14:26:51 +0000

    Add handling for invalid operation;

--- calculator.rb
+++ calculator.rb
@@ -1,11 +1,23 @@
 #!/usr/bin/env ruby
 
+class Arithmetic
+  def valid_operation?(op)
+    %w[+ - / *].include? op
+  end
+end
+
 class Calculator
+  def initialize()
+    @math = Arithmetic.new()
+  end
+
   def main()
     opt = nil
     while 'exit' != opt
       print 'Operation: '
       operation = gets.chomp.strip()
+
+      raise 'Invalid operation' unless @math.valid_operation?(operation)
 
       print 'First Number: '
       x = Float(gets.chomp)
